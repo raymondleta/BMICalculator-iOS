5a3a9843e6f027dc062ed8f9622eb1da8ffcdcad
raymond gitonga <raytosh95@gmail.com>
2019-12-27 01:18:06 +0300

    Make slider functional

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f",sender.value) + "m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text =  String(format: "%.0f",sender.value) + "kg"
+    }
+    
 }
 
